ff75f98fec4c95101912bb5fc4a599b915c271bc
Andrii <youdronveduta@gmail.com>
2024-10-08 13:53:52 +0200

    work with auth0

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import MainNav from "./main-nav";
 
+import AuthNav from './auth-nav';
+
 const NavBar = () => {
   return (
     <div className="nav-container mb-3">
@@ -9,6 +11,7 @@ const NavBar = () => {
         <div className="container">
           <div className="navbar-brand logo" />
           <MainNav />
+          <AuthNav />
         </div>
       </nav>
     </div>
